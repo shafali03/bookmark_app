c9eb5bec5c5cfa5efc29ab912834f65d4f9e66da
shafali03 <shaf03sm@gmail.com>
2019-12-02 15:02:46 +0000

    feature pass for bookmarks

--- app.rb
+++ app.rb
@@ -6,6 +6,11 @@ class BookmarkManager < Sinatra::Base
     erb :index
   end
 
+  get '/bookmarks' do
+    erb :bookmarks
+  end
+
+  
   run! if app_file == $0
 
 end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,7 +1,6 @@
 feature "viewing bookmarks" do
-  scenario "visiting the index page" do
-    visit '/'
-    expect(page).to have_content "Bookmark Manager"
+  scenario "visiting bookmark page" do
+    visit '/bookmarks'
+    expect(page).to have_content('bookmarks')
   end
-
 end
